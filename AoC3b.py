50cd783de9bd0d7beca4d02dc791c583f51144e1
kevvyb <64712618+kevvyb@users.noreply.github.com>
2020-12-03 16:03:16 -0500

    Add files via upload

--- AoC3b.py
+++ AoC3b.py
@@ -3,11 +3,6 @@ flist = []
 for forestrows in forest:
     flist.append(forestrows)
 
-#y = row, where top row is 1
-#x = index in line
-
-
-
 #forest = ["..##.......","#...#...#..",".#....#..#.","..#.#...#.#",".#...##..#.","..#.##.....",".#.#.#....#",".#........#","#.##...#...","#...##....#",".#..#...#.#"]
 
 runs = [1,3,5,7,1]
@@ -16,16 +11,12 @@ treeList = []
 
 slopecnt = 0
 for run in runs:
-    print("Run:", run)
-
     rise = rises[slopecnt]
-    print("Rise:", rise)
     #we have the x move and y move (run, rise)
     rownum = 0
     colnum = 0
     treeCnt = 0
     for row in flist:
-            print(rownum, rise)
             #only assess line if it is a multiple of the rise
             if rownum % rise != 0:
                 rownum = rownum + 1
